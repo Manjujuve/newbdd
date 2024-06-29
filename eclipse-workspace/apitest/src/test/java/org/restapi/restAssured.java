526276f58dc92820c683b3b6d6e5f434eed26425
manjujuve <manju.juve@gmail.com>
2024-06-29 23:01:43 +0530

    done upto authen

--- eclipse-workspace/apitest/src/test/java/org/restapi/restAssured.java
+++ eclipse-workspace/apitest/src/test/java/org/restapi/restAssured.java
@@ -1,5 +1,8 @@
 package org.restapi;
 
+import java.io.File;
+
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 //import org.junit.Test;
@@ -24,15 +27,19 @@ public class restAssured {
 		System.out.println(response.asPrettyString());
 	}
 	
-	@Test(enabled = false)
+	@Test
 	public void restAssuredPost() {
 		
 		RestAssured.baseURI = "https://reqres.in/"; 
 		
-		RequestSpecification requestSpecification = RestAssured.given().header("Content-Type","application/json").body("{\r\n"
-				+ "    \"name\": \"morpheus\",\r\n"
-				+ "    \"job\": \"leader\"\r\n"
-				+ "}");
+		File jsonFile = new File("src/test/java/org/restapi/datapost.json");
+		
+		RequestSpecification requestSpecification = RestAssured.given().header("Content-Type","application/json").body(jsonFile);
+		
+		//RequestSpecification requestSpecification = RestAssured.given().header("Content-Type","application/json").body("{\r\n"
+		//		+ "    \"name\": \"morpheus\",\r\n"
+		//		+ "    \"job\": \"leader\"\r\n"
+		//		+ "}");
 		
 		Response response = requestSpecification.request(Method.POST,"api/users");
 		
@@ -41,22 +48,39 @@ public class restAssured {
 		
 	}
 	
-	@Test
+	@Test(enabled = false)
 	public void restAssuredPut() {
 		
 		RestAssured.baseURI = "https://reqres.in/";
 		
 		RequestSpecification requestSpecification = RestAssured.given().header("Content-Type","application/json").body("{\r\n"
-				+ "    \"name\": \"morpheus\",\r\n"
+			+ "    \"name\": \"morpheus\",\r\n"
 				+ "    \"job\": \"zion resident\"\r\n"
 				+ "}");
-				
+		
 		
 		Response response = requestSpecification.request(Method.PUT,"api/users/2");
 		
 		System.out.println(response.statusLine());
 		System.out.println(response.asPrettyString());
 	}
+	
+	//site not working
+	@Test(enabled = false)
+	public void AuthenticationBasics()
+	{
+		RestAssured.baseURI = "https://restapi.demoqa.com/authentication/CheckForAuthentication";
+		
+		RequestSpecification request = RestAssured.given();
+
+		Response response = request.get();
+		
+		System.out.println("Status code: " + response.getStatusCode());
+		System.out.println("Status message " + response.body().asString());
+		
+	}
+	
+	
 	
 	
 	
